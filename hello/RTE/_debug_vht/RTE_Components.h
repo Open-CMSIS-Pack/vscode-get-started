b50dadc10321dee2d569d0da5ac3870cda729723
Joachim Krech <Joachim.Krech@arm.com>
2023-02-24 16:15:17 +0100

    add missing files

---
+++ hello/RTE/_debug_vht/RTE_Components.h
@@ -0,0 +1,25 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'debug+vht' 
+ * Target:  'debug+vht' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "ARMCM3.h"
+
+/* ARM::CMSIS:RTOS2:Keil RTX5:Source:5.5.4 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+
+
+#endif /* RTE_COMPONENTS_H */
